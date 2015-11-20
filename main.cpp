70612ed97439777f430ab1abba99276e58eccb52
wdy0401 <wdy0401@gmail.com>
2015-11-20 17:15:03 +0800

    添加注释

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 #include <QCoreApplication>
 #include <QProcess>
-#include <QDebug>
+#include <QTextStream>
 #include <iostream>
 #include <QFile>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QProcess gzip;
-    gzip.start("gzip", QStringList() << "-d" << "-c" << "1.txt.gz");
-    if (!gzip.waitForStarted())
+    gzip.start("gzip", QStringList() << "-d" << "-c" << argv[1]);
+    if (!gzip.waitForStarted())//正常启动and emit started singal时不返回 不正常时候直接返回错误
+    {
         return false;
+    }
+    gzip.closeWriteChannel();//不close读不出数据
 
-    gzip.closeWriteChannel();
-
-    if (!gzip.waitForFinished())
+    if (!gzip.waitForFinished())//正常结束and emit finished signal时不返回 不正常时候直接返回错误
+    {
         return false;
-
+    }
     QTextStream in(&gzip);
-    while (!in.atEnd()) {
-        QString line = in.readLine();
-        //qDebug()<<"##"<<line<<"##\n";
-        std::cout<<"##"<<line.toStdString()<<"##"<<std::endl;
-        }
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();//已经去掉末尾的空白
+        std::cout<<line.toStdString()<<std::endl;
+    }
     return 0;
-//    return a.exec();
 }
